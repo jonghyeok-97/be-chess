895cb9c5a935df9b40156b45c4354787e9571f32
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-05 14:45:11 +0900

    test: ValueSource를 EnumSource로 변경
    Pawn 객체는 Color 라는 Enum을 사용해서 생성되기 때문에 ValueSource의 strings 를 EnumSource의 Color.class로 변경

--- chess1/src/test/java/chess/piece/PawnTest.java
+++ chess1/src/test/java/chess/piece/PawnTest.java
@@ -2,6 +2,7 @@ package chess.piece;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
@@ -10,7 +11,7 @@ class PawnTest {
 
     @ParameterizedTest
     @DisplayName("폰의 색깔은 흰색과 검정색이어야 한다.")
-    @ValueSource(strings = {"WHITE", "BLACK"})
+    @EnumSource(value = Color.class, names = {"WHITE", "BLACK"})
     void create(Color color) {
         assertThatCode(() -> new Pawn(color))
                 .doesNotThrowAnyException();
