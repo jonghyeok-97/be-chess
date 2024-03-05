06170e0211bc2402ad933480f7f575f8350a5418
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-05 15:11:20 +0900

    feat: Pawn에 색깔이 없으면 생성되지 않는 기능 구현
    Pawn의 색깔이 null일 경우, 예외가 발생하도록 지정

--- chess1/src/main/java/chess/piece/Pawn.java
+++ chess1/src/main/java/chess/piece/Pawn.java
@@ -4,6 +4,9 @@ class Pawn {
     private final Color color;
 
     Pawn(final Color color) {
+        if (color == null) {
+            throw new IllegalArgumentException("색깔을 정해야 합니다.");
+        }
         this.color = color;
     }
 
--- chess1/src/test/java/chess/piece/PawnTest.java
+++ chess1/src/test/java/chess/piece/PawnTest.java
@@ -1,10 +1,11 @@
 package chess.piece;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 class PawnTest {
@@ -12,8 +13,15 @@ class PawnTest {
     @ParameterizedTest
     @DisplayName("폰의 색깔은 흰색과 검정색이어야 한다.")
     @EnumSource(value = Color.class, names = {"WHITE", "BLACK"})
-    void create(Color color) {
+    void createPawnValidColor(Color color) {
         assertThatCode(() -> new Pawn(color))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("폰의 색깔이 없으면 폰은 생성되지 않는다")
+    void createPawnNoColor() {
+        assertThatThrownBy(() -> new Pawn(null))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
