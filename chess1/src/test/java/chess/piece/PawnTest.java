bdb032af4fe1ba90fc293fc58b97ad82db0d3bfd
jonghyeok <dkswhdgur1209@naver.com>
2024-03-05 10:35:27 +0900

    [그로밋] step-1 체스 프로젝트 시작 (#33)
    * chore: 프로젝트 관리에 용이하도록 Gradle 추가 및 gitignore파일 추가
    
    개발환경에 종속적이지 않도록 Gradle 추가, .gitignore파일을 사용해서 git으로 관리하지 않을 대상 제외
    
    * feat: 폰의 색깔은 검정색과 흰색
    
    폰의 색깔은 흰색과 검은색이어야 한다.
    
    * chore: Gradle test시 한글 깨지지 않도록 변경
    
    Windows-949 인코딩을 사용해서 한글이 깨지는 것을 UTF-8로 변경해서 막기 위해 Gradle에 인코딩 설정 추가
    
    * refactor: Pawn의 색깔은 하얀색과 검정색만 들어오도록 테스트 코드를 수정
    
    Pawn이 흰색과 검정색으로 생성되면 아무 문제가 없다는 것을 코드로 표현

---
+++ chess1/src/test/java/chess/piece/PawnTest.java
@@ -0,0 +1,18 @@
+package chess.piece;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+class PawnTest {
+
+    @ParameterizedTest
+    @DisplayName("폰의 색깔은 흰색과 검정색이어야 한다.")
+    @ValueSource(strings = {"WHITE", "BLACK"})
+    void create(Color color) {
+        assertThatCode(() -> new Pawn(color))
+                .doesNotThrowAnyException();
+    }
+}
