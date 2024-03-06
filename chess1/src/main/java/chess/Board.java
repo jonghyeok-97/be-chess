3746832c18d70af5c87ab27c152bc6f5645b2777
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-06 10:45:23 +0900

    feat: 폰만 갖고 있는 체스 판 초기화

--- chess1/src/main/java/chess/Board.java
+++ chess1/src/main/java/chess/Board.java
@@ -2,8 +2,17 @@ package chess;
 
 import chess.pieces.Pawn;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Board {
     private final List<Pawn> pawns;
@@ -15,6 +24,19 @@ public class Board {
     private Board(final List<Pawn> pawns) {
         this.pawns = pawns;
     }
+
+    Board initialize() {
+        final List<Pawn> blackPawns = IntStream.rangeClosed(1, 8)
+                .mapToObj(i -> Pawn.createBlackPawn())
+                .collect(Collectors.toList());
+        final List<Pawn> whitePawns = IntStream.rangeClosed(1, 8)
+                .mapToObj(i -> Pawn.createWhitePawn())
+                .toList();
+        blackPawns.addAll(whitePawns);
+        return new Board(blackPawns);
+    }
+
+
 
     Board add(final Pawn pawn) {
         final List<Pawn> adding = new ArrayList<>(pawns);
