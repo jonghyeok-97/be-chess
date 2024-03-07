d2a5093125b47f4d4c86da5f1d2eae3e357b738f
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-07 15:36:37 +0900

    feat: 기물들을 추가하여 값 객체로 변경

--- chess1/src/main/java/chess/pieces/Piece.java
+++ chess1/src/main/java/chess/pieces/Piece.java
@@ -28,7 +28,59 @@ public class Piece {
         return new Piece(Color.BLACK, PieceType.Knight);
     }
 
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, PieceType.Rook);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Color.BLACK, PieceType.Rook);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, PieceType.Bishop);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Color.BLACK, PieceType.Bishop);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, PieceType.Queen);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Color.BLACK, PieceType.Queen);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, PieceType.King);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Color.BLACK, PieceType.King);
+    }
+
+//    public boolean isSameColor(final Color color) throws ColorException{
+//        if (color == null) {
+//            throw new ColorException("color는" + color + "가 될 수 없습니다");
+//        }
+//        return this.color == color;
+//    }
+
     public boolean isBlack() {
         return color == Color.BLACK;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Piece piece = (Piece) o;
+        return color == piece.color && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, type);
     }
 }
