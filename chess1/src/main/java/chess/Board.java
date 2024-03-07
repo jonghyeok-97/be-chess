f8ab16ac9f2f7cd221159394c9f45795cf560be9
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-07 10:21:17 +0900

    chore: 사용하지 않는 import제거

--- chess1/src/main/java/chess/Board.java
+++ chess1/src/main/java/chess/Board.java
@@ -1,19 +1,9 @@
 package chess;
 
-import chess.pieces.Color;
 import chess.pieces.Pawn;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
--- chess1/src/test/java/chess/pieces/PawnTest.java
+++ chess1/src/test/java/chess/pieces/PawnTest.java
@@ -2,8 +2,6 @@ package chess.pieces;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
